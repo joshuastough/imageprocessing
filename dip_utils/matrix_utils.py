ee23c9acbef8fcfcc14a604be988f9748cc4e2f3
Joshua Stough <jvs008@bucknell.edu>
2021-02-17 12:25:36 -0500

    new enhancement codes

--- dip_utils/matrix_utils.py
+++ dip_utils/matrix_utils.py
@@ -7,4 +7,18 @@ import numpy as np
 
 # Given an ndarray, returns a tuple of info...
 def arr_info(arr):
+    """
+    arr_info(arr): return a tuple of shape, type, min, and max for the ndarray arr.
+    """
     return arr.shape, arr.dtype, arr.min(), arr.max()
+
+
+def make_linmap(inputrange=[0,1], outputrange=[0,255]):
+    '''
+    make_linmap(inputrange=[0,1], outputrange=[0,255]): return an anonymous function
+    linear mapping from the inputrange to the outputrange.
+    '''
+    a,b = inputrange
+    c,d = outputrange
+    
+    return lambda x: (1-((x-a)/(b-a)))*c + ((x-a)/(b-a))*d
